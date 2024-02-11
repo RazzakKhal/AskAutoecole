a47f3aa2c91b7adeeadc4d2a45019b51e78eb585
Sofiane KHALFALLAH <sofiane@MacBook-Air-de-Sofiane.local>
2024-02-11 10:09:30 +0100

    chore: version mobile ok

--- src/app/shared/constantes/constantes.ts
+++ src/app/shared/constantes/constantes.ts
@@ -1,2 +1,2 @@
-export const FORMULES_WIDTH = window.innerWidth * 0.90;
+export const FORMULES_WIDTH = window.innerWidth < 576 ? window.innerWidth * 0.90 : window.innerWidth * 0.45;
 export const FORMULES_MARGIN = 10;
